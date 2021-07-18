385cd6cb87093055c3f510f903364f91af60e842
Omer Bresinski <omer@powerlink.co.il>
2021-07-18 16:54:37 +0300

    Supports cookie

--- src/PowerlinkAPI.ts
+++ src/PowerlinkAPI.ts
@@ -1,40 +1,60 @@
 import { QueryParams, ConvertedQueryParams } from "./types";
 import axios from "axios";
 export class PowerlinkAPI {
-  private token: string;
+  private token?: string;
   private baseUrlV1 = "https://api.powerlink.co.il/api/";
   private baseUrlV2 = "https://api.powerlink.co.il/api/v2/";
 
-  constructor(token: string) {
+  constructor(token?: string) {
     this.token = token;
   }
 
   async get(params: QueryParams) {
     const convertedQueryParams = this.getConvertedParams(params);
     try {
-      const response = await axios.post(
-        `${this.baseUrlV1}/query`,
-        convertedQueryParams,
-        {
-          headers: { tokenid: this.token },
-        }
-      );
-      return response.data;
+      if (this.token) {
+        const response = await axios.post(
+          `${this.baseUrlV1}/query`,
+          convertedQueryParams,
+          {
+            headers: { tokenid: this.token },
+          }
+        );
+        return response.data;
+      } else {
+        const response = await axios.post(
+          `${this.baseUrlV1}/query`,
+          convertedQueryParams
+        );
+        return response.data;
+      }
     } catch (ex) {
       console.log(ex);
     }
   }
 
   async update(objectType: number, objectId: string, data: object) {
-    try {
-      const updatedRecord = await axios.put(
-        `${this.baseUrlV2}/record/${objectType}/${objectId}`,
-        data,
-        { headers: { tokenid: this.token } }
-      );
-      return updatedRecord.data;
-    } catch (ex) {
-      console.log(ex);
+    if (this.token) {
+      try {
+        const updatedRecord = await axios.put(
+          `${this.baseUrlV2}/record/${objectType}/${objectId}`,
+          data,
+          { headers: { tokenid: this.token } }
+        );
+        return updatedRecord.data;
+      } catch (ex) {
+        console.log(ex);
+      }
+    } else {
+      try {
+        const updatedRecord = await axios.put(
+          `${this.baseUrlV2}/record/${objectType}/${objectId}`,
+          data
+        );
+        return updatedRecord.data;
+      } catch (ex) {
+        console.log(ex);
+      }
     }
   }
 
