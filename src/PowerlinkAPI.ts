767985b7730433affb9d4ed2f0183048371a3c86
Omer Bresinski <omer@powerlink.co.il>
2021-07-18 13:21:23 +0300

    Get & Update

--- src/PowerlinkAPI.ts
+++ src/PowerlinkAPI.ts
@@ -1,6 +1,54 @@
+import { QueryParams, ConvertedQueryParams } from "./types";
+import axios from "axios";
 export class PowerlinkAPI {
-  constructor(token: string) {}
-  query() {
-    console.log("query");
+  private token: string;
+  private baseUrl = "https://api.powerlink.co.il/api";
+
+  constructor(token: string) {
+    this.token = token;
   }
+
+  async get(params: QueryParams) {
+    const convertedQueryParams = this.getConvertedParams(params);
+    try {
+      const response = await axios.post(
+        `${this.baseUrl}/query`,
+        convertedQueryParams,
+        {
+          headers: { tokenid: this.token },
+        }
+      );
+      console.log(response.data);
+    } catch (ex) {
+      console.log(ex);
+    }
+  }
+
+  update(objectType: number, objectId: string, data: object) {
+    axios.put(
+      `https://api.powerlink.co.il/api/record/${objectType}/${objectId}`,
+      data,
+      { headers: { tokenid: this.token } }
+    );
+  }
+
+  getConvertedParams = ({
+    objectType,
+    pageSize,
+    pageNumber,
+    conditions,
+    fields,
+    sortBy,
+    sortType,
+  }: QueryParams): ConvertedQueryParams => {
+    return {
+      objecttype: objectType,
+      page_size: pageSize,
+      page_number: pageNumber,
+      query: conditions,
+      sort_by: sortBy,
+      sort_type: sortType,
+      fields,
+    };
+  };
 }
---
+++ src/types.ts
@@ -0,0 +1,21 @@
+export interface QueryParams {
+  pageSize: number;
+  objectType: number;
+  pageNumber: number;
+  sortBy?: string;
+  fields?: string;
+  conditions?: string;
+  sortType?: sortType;
+}
+
+export interface ConvertedQueryParams {
+  objecttype: number;
+  page_size: number;
+  page_number: number;
+  fields?: string;
+  query?: string;
+  sort_by?: string;
+  sort_type?: sortType;
+}
+
+type sortType = "desc" | "asc";
