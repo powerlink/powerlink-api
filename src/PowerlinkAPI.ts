3b0ebff3aa7091ff2e33adbd52341211f59c0814
Omer Bresinski <omer@powerlink.co.il>
2021-07-20 13:12:11 +0300

    Removed old code

--- src/PowerlinkAPI.ts
+++ src/PowerlinkAPI.ts
@@ -29,7 +29,6 @@ export class plapi {
         return response.data;
       } else {
         const response = await this.api("get", convertedQueryParams);
-        // const response = await window.parent.plapi.query(convertedQueryParams);
         return response;
       }
     } catch (ex) {
@@ -76,7 +75,7 @@ export class plapi {
         return response;
       }
     } catch (ex) {
-      // console.log(ex);
+      console.log(ex);
     }
   }
   async create(data: object, objectType: number) {
